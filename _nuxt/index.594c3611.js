4e7a07bb6f15472bff2f0f3a1356ec79b59fdce7
TânDv <mchemistry95@gmail.com>
2023-02-23 02:59:56 +0700

    Updates

---
+++ _nuxt/index.594c3611.js
@@ -0,0 +1,1 @@
+import{i as o,o as t,b as s,r as n}from"./entry.2a22bff4.js";const r={},c={class:"container mx-auto"};function a(e,_){return t(),s("div",c,[n(e.$slots,"default")])}const i=o(r,[["render",a]]);export{i as default};

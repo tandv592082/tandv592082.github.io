8de2d6a6f176ac08759803a323a75ece324ca8a1
TânDv <mchemistry95@gmail.com>
2023-02-26 22:21:33 +0700

    chore: fix flash fixed bar

--- app.vue
+++ app.vue
@@ -29,7 +29,7 @@ useHead({
 <template>
   <Html :class="`${currentTheme === 'dark' ? 'dark' : ''}`" :lang="locale">
 
-  <Body class="antialiased dark:bg-dark-800 tracking-wide font-size transition-colors text-gray-700 dark:text-gray-200">
+  <Body class="antialiased dark:bg-dark-800 tracking-wide font-size  text-gray-700 dark:text-gray-200">
     <NuxtLayout>
       <NuxtLoadingIndicator />
       <NuxtPage />

1e0052c58b3e5df448c815c49b2f87c4353d2693
TânDv <mchemistry95@gmail.com>
2023-02-26 22:17:46 +0700

    chore: fixed settings

--- layouts/index.vue
+++ layouts/index.vue
@@ -7,12 +7,12 @@ const CVThemeSwitcher = resolveComponent('CVThemeSwitcher');
 
 <template>
     <div class="container mx-auto mb-10">
-        <div class="h-12 flex justify-end items-center">
+        <div class="h-12 flex justify-end items-center fixed bg-white dark:bg-dark-800 w-full container <sm:justify-center">
             <CVDownload class="mr-5"/>
             <CVLangSwitcher class="mr-5"/>
             <CVThemeSwitcher />
         </div>
-        <div id="__cv" class="mx-auto content w-prose px-4 py-1 rounded-md">
+        <div id="__cv" class="mx-auto content w-prose px-4 py-1 rounded-md pt-12">
             <slot />
         </div>
     </div>

2fbccd3bc3660c4454c78705f988105e541ac58a
Tân DV <mchemistry95@gmail.com>
2023-02-28 01:44:35 +0700

    chore(i18n): update translate data.

--- i18n/en/_cv.ts
+++ i18n/en/_cv.ts
@@ -7,13 +7,8 @@ export default {
         linkedin: 'https://www.linkedin.com/in/tan-duong-van-106789126/'
     },
     summary:{
-        0: `Backend Engineer with over 4 years of experience in developing and maintaining complex software
-            applications using Java and Spring Boot. Extensive experience in deploying WebService systems
-            for monitoring and controlling self-driving cars and drones, communicating with robot systems 
-            (ROS) through MQTT protocol, and integrating them into the WebService system. 
-            Also experienced in implementing RESTful APIs for both mobile and web. With over 1 year of experience in 
-            Frontend development using React Native, Vue.js, and optimizing WordPress sites.`,
-        1: `I am always dedicated and serious in my work and constantly seeking to improve my skills and knowledge through research.`
+        0: `Backend engineer with over 4 years of experience in developing and maintaining Web Service systems using NodeJS and MongoDB. Also experienced in collecting data for monitoring and controlling self-driving cars, unmanned aerial vehicles through communication with robot systems (ROS) using MQTT protocol. Additionally, has over 1 year of experience in developing front-end applications using React Native, Vue.js, and optimizing WordPress sites.`,
+        1: `I am always dedicated and serious about my work to create value for the company. At the same time, I always seek to learn and improve my skills and knowledge to become a good engineer.`
     },
     skills: {
         0: {
@@ -26,7 +21,7 @@ export default {
         },
         2: {
             title: `Frameworks/Platforms`,
-            details: 'NodeJS, Docker, Redis, ReactJS, React Native, VueJS, JQuery, WebSocket, Wordpress.',
+            details: 'NodeJS (Express, Fastify), Docker, Redis, ReactJS, React Native, VueJS, JQuery, WebSocket, Wordpress, CI/CD with Gitlab.',
         },
         3: {
             title: `Version Control`,
@@ -41,9 +36,12 @@ export default {
             details: 'Linux, VPS, Linux server, Window.',
         },
         6: {
-            details: 'Familiar with Agile/Scrum development methodologies.',
+            details: 'Experience with RESTful API design and implementation.',
         },
         7: {
+            details: 'Familiar with Agile/Scrum development methodologies.',
+        },
+        8: {
             details: 'Mentoring and code reviews',
         },
     },
@@ -61,12 +59,15 @@ export default {
             project: {
                 title: 'X Bus and Drone in a Box (DIB) Web Service.',
                 description: 'Both X Bus and DIB Web Service are systems that help to collect data, manage and give commands to the real-time robot system.',
-                technologies: 'Nodejs, MongoDB, Redis, Docker, MQTT, React Native, Vuejs.',
+                technologies: 'Nodejs(Express, Fastify), MongoDB, Redis, Docker, MQTT, React & Redux, Vuejs & VueX, WebSocket.',
                 responsibilities: {
-                    0: `Developed and deployed WebService system for monitoring and controlling self-driving cars and drones, ensuring smooth communication between the systems and the control center`,
-                    1: `Implemented communication between the backend and Robot Operating System (ROS) to exchange data and control signals using MQTT protocol.`,
-                    2: `Improved application performance by optimizing database queries and implementing caching mechanisms`,
-                    3: `Implemented industry best practices, such as data validation and error handling, to ensure the reliability and security of the applications.`
+                    0: `Define the architecture, coding convention, and setup CI/CD system for all front-end and back-end projects (includes linter, unit test, build script using Docker).`,
+                    1: `Communicate with relevant parties to build and design the WebService system.`,
+                    2: `Design database schemas and implement back-end system to manage data from the Robot system (both XBus and DIB) and provide it to the front-end application.`,
+                    3: `Reseach and develop the communication between the back-end system and the X Bus system via the MQTT protocol to collect data and send commands.`,
+                    4: `Optimize the process of reading and writing data from WebSocket to MongoDB by improving database queries and utilizing Redis.`,
+                    5: `Reseach and develop a service on the drone to decode data, send commands, and communicate with the back-end system via WebSocket.`,
+                    6: `Help junior developers in solving issues and review code quanlity.`
                 }
             }
         },
@@ -83,11 +84,11 @@ export default {
             project: {
                 title: 'FADI Web Service.',
                 description: 'A system that helps to link and track customer contact data from WordPress pages, as well as improve the workflow of the company.',
-                technologies: 'Nodejs, MongoDB, Docker, ReactJS, VueJS, Wordpress.',
+                technologies: 'Nodejs(Express), MongoDB, Docker, VueJS & VueX, Wordpress, Webpack.',
                 responsibilities: {
-                    0: `Developed and deployed WebService system for monitoring and controlling self-driving cars and drones, ensuring smooth communication between the systems and the control center`,
-                    1: `Implemented communication between the backend and Robot Operating System (ROS) to exchange data and control signals using MQTT protocol.`,
-                    2: `Improved application performance by optimizing database queries and implementing caching mechanisms`,
+                    0: `Define the architecture, coding convention front-end and back-end projects (includes linter, unit test, build script using Docker).`,
+                    1: `Communicate with superiors to design and build a back-end system to optimize the workflow of the company.`,
+                    2: `Designing the database schemas and implementing the main modules of the back-end.`,
                     3: `Refactor the interface and optimizing the loading speed of the company's WordPress website. Linking user contact data with the backend through API.`
                 }
             }
@@ -105,7 +106,7 @@ export default {
             project: {
                 title: 'ABAGolf, Feeby.',
                 description: 'ABAGolf is a golf application designed for golf players, which helps organize tournaments and view scores in real-time, automatically calculate handicaps, book golf courses, and create chat groups. Feeby is an application that allows customers to review and receive discount codes after using the services of a store.',
-                technologies: 'Nodejs, MongoDB, Vuejs, React Native, Bootstrap.',
+                technologies: 'Nodejs(Express), MongoDB, Vuejs & VueX, React Native & Redux, Bootstrap.',
                 responsibilities: {
                     0: `Developed an app interface based on Figma design using React Native.`,
                     1: `Implemented Redux for state management and integrated with RESTf API for data retrieval.`,
--- i18n/vi/_cv.ts
+++ i18n/vi/_cv.ts
@@ -7,8 +7,8 @@ export default {
         linkedin: 'https://www.linkedin.com/in/tan-duong-van-106789126/'
     },
     summary:{
-        0: `Kỹ sư Backend với hơn 4 năm kinh nghiệm trong phát triển và duy trì các ứng dụng phần mềm phức tạp bằng Java và Spring Boot. Có kinh nghiệm rộng trong triển khai các hệ thống WebService để giám sát và điều khiển xe tự lái và máy bay không người lái, giao tiếp với hệ thống robot (ROS) qua giao thức MQTT, và tích hợp chúng vào hệ thống WebService. Cũng có kinh nghiệm trong triển khai các API RESTful cho cả di động và web. Với hơn 1 năm kinh nghiệm trong phát triển Frontend sử dụng React Native, Vue.js, và tối ưu hóa các trang WordPress`,
-        1: `Tôi luôn tận tâm và nghiêm túc trong công việc của mình và liên tục tìm cách nâng cao kỹ năng và kiến thức của mình thông qua nghiên cứu.`
+        0: `Kỹ sư Backend với hơn 4 năm kinh nghiệm trong phát triển và duy trì các hệ thống Web Service bằng NodeJS và MongoDB. Cũng có kinh nghiệm trong việc thu thập dữ liệu nhằm giám sát, điều khiển xe tự lái, máy bay không người lái thông qua giao tiếp với hệ thống robot (ROS) bằng giao thức MQTT. Đồng thời có hơn 1 năm kinh nghiệm trong phát triển các ứng dụng phía front-end sử dụng React Native, Vue.js, và tối ưu hóa các trang WordPress.`,
+        1: `Tôi luôn tận tâm và nghiêm túc với công việc để tạo ra giá trị cho công ty. Đồng thời luôn tìm tòi học hỏi, trau dồi kiến ​​thức, kỹ năng để trở thành một kỹ sư giỏi.`
     },
     skills: {
         0: {
@@ -21,7 +21,7 @@ export default {
         },
         2: {
             title: `Frameworks/Platforms`,
-            details: 'NodeJS, Docker, Redis, ReactJS, React Native, VueJS, JQuery, WebSocket, Wordpress.',
+            details: 'NodeJS (Express, Fastify), Docker, Redis, ReactJS, React Native, VueJS, JQuery, WebSocket, Wordpress, CI/CD with Gitlab.',
         },
         3: {
             title: `Version Control`,
@@ -36,9 +36,12 @@ export default {
             details: 'Linux, VPS, Linux server, Window.',
         },
         6: {
-            details: 'Quen thuộc với các phương pháp phát triển Agile/Scrum.',
+            details: 'Có kinh nghiệm thiết kế và triển khai RESTful API.',
         },
         7: {
+            details: 'Quen thuộc với các phương pháp phát triển Agile/Scrum.',
+        },
+        8: {
             details: 'Cố vấn và đánh giá chất lượng code.',
         },
     },
@@ -56,12 +59,15 @@ export default {
             project: {
                 title: 'X Bus và Drone in a Box (DIB) Web Service.',
                 description: 'Triển khai các ứng dụng theo dõi, điều khiển và web service cho xe tự lại và máy bay không người lại',
-                technologies: 'Nodejs, MongoDB, Redis, Docker, MQTT, React Native, Vuejs.',
+                technologies: 'Nodejs(Express, Fastify), MongoDB, Redis, Docker, MQTT, React & Redux, Vuejs & VueX, WebSocket.',
                 responsibilities: {
-                    0: `Developed and deployed WebService system for monitoring and controlling self-driving cars and drones, ensuring smooth communication between the systems and the control center`,
-                    1: `Triển khai giao tiếp giữa hệ thống backend và ROS thông qua giao thức MQTT.`,
-                    2: `Tối ưu quá trình lưu dữ liệu web socket vào MongoDB`,
-                    3: `Implemented industry best practices, such as data validation and error handling, to ensure the reliability and security of the applications.`
+                    0: `Xây dựng cấu ​​trúc, quy ước viết code và thiết lập hệ thống CI/CD cho tất cả các dự án front-end và back-end (bao gồm linter, unit test, build script bằng Docker).`,
+                    1: `Giao tiếp với các bên liên quan để xây dựng và thiết kế hệ thống WebService.`,
+                    2: `Thiết kế lược đồ cơ sở dữ liệu và triển khai hệ thống back-end để quản lý dữ liệu từ hệ thống Robot(cả XBUS và DIB) và cung cấp cho ứng dụng front-end.`,
+                    3: `Triển khai giao tiếp giữa hệ thống back-end và hệ thống Robot thông qua giao thức MQTT nhằm thu thập dữ liệu và ra lệnh.`,
+                    4: `Tối ưu quá trình đọc, ghi dữ liệu từ Web Socket vào MongoDB bằng cách cải thiện các truy vấn cơ sở dữ liệu và sử dụng Redis.`,
+                    5: `Nghiên cứu và xây dựng service trên drone nhằm giải mã thông tin dữ liệu, ra lệnh và giao tiếp với hệ thống back-end thông qua WebSocket.`,
+                    6: `Giúp đỡ đồng nghiệp cấp dưới giải quyết các vấn đề và đánh giá chất lượng code.`
                 }
             }
         },
@@ -76,14 +82,14 @@ export default {
                 period: '02/2019 - 02/2021',
             },
             project: {
-                title: 'FADI Web Service',
+                title: 'FADI Web Service.',
                 description: 'A system that helps to link and track customer contact data from WordPress pages, as well as improve the workflow of the company.',
-                technologies: 'Nodejs, MongoDB, Docker, ReactJS, VueJS, Wordpress.',
+                technologies: 'Nodejs(Express), MongoDB, Docker, VueJS & VueX, Wordpress, Webpack.',
                 responsibilities: {
-                    0: `Developed and deployed WebService system for monitoring and controlling self-driving cars and drones, ensuring smooth communication between the systems and the control center`,
-                    1: `Implemented communication between the backend and Robot Operating System (ROS) to exchange data and control signals using MQTT protocol.`,
-                    2: `Improved application performance by optimizing database queries and implementing caching mechanisms`,
-                    3: `Optimize the interface and system of the company's WordPress website.`
+                    0: `Xây dựng cấu ​​trúc, quy ước viết code cho các dự án front-end và back-end (bao gồm linter, unit test, build script bằng Docker).`,
+                    1: `Giao tiếp với cấp trên để thiết kế và xây dựng hệ thống back-end để tối ưu hóa quy trình làm việc của công ty.`,
+                    2: `Thiết kế lược đồ cơ sở dữ liệu và triển khai các module chính của phía back-end.`,
+                    3: `Cấu trúc lại hệ thống giao diện và tối ưu hóa tốc độ tải WordPress của công ty. Liên kế dữ liệu liên hệ của người dùng thông qua API.`
                 }
             }
         },
@@ -100,7 +106,7 @@ export default {
             project: {
                 title: 'ABAGolf, Feeby.',
                 description: 'ABAGolf là ứng dụng dành cho người chơi gôn, nó giúp tổ chức giải và xem điểm trực tiếp,  tính HDC tự động, đặt sân gôn và có thể tạo nhóm trò chuyện. Feeby là ứng dụng giúp  khách hàng đánh giá và nhận mã khuyến mãi sau khi sử dụng dịch vụ của cửa hàng.',
-                technologies: 'Nodejs, MongoDB, Vuejs, React Native, Bootstrap.',
+                technologies: 'Nodejs(Express), MongoDB, Vuejs & VueX, React Native & Redux, Bootstrap.',
                 responsibilities: {
                     0: `Phát triển giao diện ứng dụng dựa trên Figma sử dụng React Native.`,
                     1: `Triển khai Redux để quản lý trạng thái và tích hợp với REST API để truy xuất dữ liệu.`,
